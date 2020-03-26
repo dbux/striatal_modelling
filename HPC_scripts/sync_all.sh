658403891047e9898a35485e12bc4f4fadeb6feb
David Buxton <dbuxton+github@gmail.com>
2020-03-26 15:34:12 +0000

    Path fix

--- HPC_scripts/sync_all.sh
+++ HPC_scripts/sync_all.sh
@@ -9,9 +9,10 @@ rsync -ahirstuvz "${DATA_DIR}/Models/" "ac1drb@iceberg.sheffield.ac.uk:/data/ac1
 echo "Syncing striatums from ${DATA_DIR}/Striatums…"
 rsync -ahirstuvz "${DATA_DIR}/Striatums/" "ac1drb@iceberg.sheffield.ac.uk:/data/ac1drb/striatums"
 
-echo "Syncing bash scripts from ${DATA_DIR}/Scripts…"
-rsync -ahirstuvz "${DATA_DIR}/Scripts/" "ac1drb@iceberg.sheffield.ac.uk:/home/ac1drb/scripts"
-rsync -ahirstuvz "ac1drb@iceberg.sheffield.ac.uk:/home/ac1drb/scripts/" "${DATA_DIR}/Scripts/"
+# Scripts now synced via GitHub
+# echo "Syncing bash scripts from ${DATA_DIR}/Scripts…"
+# rsync -ahirstuvz "${DATA_DIR}/Scripts/" "ac1drb@iceberg.sheffield.ac.uk:/home/ac1drb/scripts"
+# rsync -ahirstuvz "ac1drb@iceberg.sheffield.ac.uk:/home/ac1drb/scripts/" "${DATA_DIR}/Scripts/"
 
 # MatLab now synced via GitHub
 # echo "Syncing MatLab scripts from ${DATA_DIR}/MatLab…"
