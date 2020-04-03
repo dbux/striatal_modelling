03a04bd1dc22c6174a3615e179e4e82bcde8d5e7
David Buxton <dbuxton+github@gmail.com>
2020-04-03 17:34:37 +0100

    Correct striatum size

--- generate_striatum.m
+++ generate_striatum.m
@@ -20,7 +20,7 @@ end
 % DEFINE ATTRIBUTES
 %
 % Physical striatum attributes
-attr.size        = 600;    % Size of model striatum each side (μm) (Was 250μm in Humphries et al. 2009)
+attr.size        = 1000;    % Size of model striatum each side (μm) (Was 250μm in Humphries et al. 2009)
 attr.min_dist    = 10;      % Minimum distance between neurons (μm)
 attr.centre_rad  = 75;      % Radius of central region free from edge effects (μm)
 attr.msn_density = 84900;   % Number of MSNs to place per mm^3 (should be 84,900)
