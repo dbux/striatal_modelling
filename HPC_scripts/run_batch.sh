dc6cc84d269bdc232fde54bb06496a19505d48ca
David Buxton <dbuxton+github@gmail.com>
2020-09-23 18:02:02 +0100

    Update

--- HPC_scripts/run_batch.sh
+++ HPC_scripts/run_batch.sh
@@ -58,21 +58,18 @@ while getopts ":c:f:m:w:" opt; do
 			VAR_FSI=${OPTARG}
 		  	if [ ! -z "${VAR_FSI}" ]; then
 		  		export T_STOP=`echo ${T_STOP} \* 5 | bc`
-				printf "TSTOP now ${TSTOP}"
 		  	fi
 		;;
 		m)
 			VAR_MSN=${OPTARG}
 		  	if [ ! -z "${VAR_MSN}" ]; then
 		  		export T_STOP=`echo ${T_STOP} \* 5 | bc`
-				printf "TSTOP now ${TSTOP}"
 		  	fi
 		;;
 		w) 
 			VAR_WCH=${OPTARG}
 		  	if [ ! -z "${VAR_WCH}" ]; then
 		  		export T_STOP=`echo ${T_STOP} \* 5 | bc`
-				printf "TSTOP now ${TSTOP}"
 		  	fi
 		;;
 		\?) echo "Invalid option -${OPTARG}" >&2
