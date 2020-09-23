347e49ae6e5e0f9d37768ea5ec480010b750fa91
David Buxton <dbuxton+github@gmail.com>
2020-09-23 16:48:02 +0100

    Update run_batch.sh

--- HPC_scripts/run_batch.sh
+++ HPC_scripts/run_batch.sh
@@ -2,8 +2,10 @@
 
 # Set HPC parameters
 export USER="ac1drb"
-export RMEM="30G"
-export TIME="1:30:00"
+export RMEM="3G"
+# export RMEM="30G"
+export TIME="30:00"
+# export TIME="1:30:00"
 
 # Set model name and experiment number
 export MODEL="Physical_2CH"
