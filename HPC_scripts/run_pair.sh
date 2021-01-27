0e52826dc8f0d814823c90f1176be72d5d53e9d5
David Buxton <dbuxton+github@gmail.com>
2021-01-27 15:45:50 +0000

    Update run_pair.sh

--- HPC_scripts/run_pair.sh
+++ HPC_scripts/run_pair.sh
@@ -2,8 +2,10 @@
 
 # Set HPC parameters
 export USER="ac1drb"
-export RMEM="60G"
-export TIME="1:30:00"
+# export RMEM="60G"
+export RMEM="10G"
+#export TIME="1:30:00"
+export TIME="0:30:00"
 
 # Set model name and experiment number
 export MODEL="Universal"
