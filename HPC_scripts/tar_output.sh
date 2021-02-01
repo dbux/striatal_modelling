ed72e544937e8694b535bc30007af4a49bb30c04
David Buxton <dbuxton+github@gmail.com>
2021-02-01 12:08:48 +0000

    Update tar_output.sh

--- HPC_scripts/tar_output.sh
+++ HPC_scripts/tar_output.sh
@@ -2,25 +2,42 @@
 
 # Compress output files, excluding model state files and components. (i.e. Only include logs)
 
-LABEL=$1
+# LABEL=$1
 
 # Set root directories
-if [ -d "/fastdata/ac1drb" ]; then
-	FD="/fastdata/ac1drb"
-else
-	FD="/fastdata-sharc/ac1drb"
-fi
+#if [ -d "/fastdata/ac1drb" ]; then
+#	FD="/fastdata/ac1drb"
+	#else
+#	FD="/fastdata-sharc/ac1drb"
+	#fi
 
+FD="/fastdata/ac1drb"
 NOW=$(date +%y.%m.%d_%H.%M)
-INPUT_DIR=${FD}/output/$LABEL/
+INPUT_DIR=${FD}/output
 OUTPUT_DIR=${FD}
 
-tar -zvcf $OUTPUT_DIR/output_$LABEL\_$NOW.tar.gz $INPUT_DIR \
---exclude 'model/*' \
---exclude 'model' \
---exclude 'run/*' \
---exclude 'run' \
---exclude 'output.script' \
---exclude 'run_brahms*'\
+# From https://stackoverflow.com/a/2108296
+for dir in ${INPUT_DIR}/*/		# List directories in the form "/tmp/dirname/"
+do
+	exp=${dir%*/}     		 	# Remove the trailing "/"
+	echo ${dir##*/}				# Print everything after the final "/"
+	
+	tar -C ${INPUT_DIR} -zvcf ${OUTPUT_DIR}/output_${exp}\_${NOW}.tar.gz ${exp} \
+	--exclude 'model/*' \
+	--exclude 'model' \
+	--exclude 'run/*' \
+	--exclude 'run' \
+	--exclude 'output.script' \
+	--exclude 'run_brahms*'
+	
+done
+
+#tar -C ${INPUT_DIR} -zvcf ${OUTPUT_DIR}/output_${LABEL}\_${NOW}.tar.gz ${LABEL} \
+#--exclude 'model/*' \
+#--exclude 'model' \
+#--exclude 'run/*' \
+#--exclude 'run' \
+#--exclude 'output.script' \
+#--exclude 'run_brahms*'
 
 echo "Done!"
